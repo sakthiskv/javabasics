3bfd83c020d01f96710c9b74c3e0abd4ad1be8c0
sakthiskv <romansakthi18@gmail.com>
2018-05-29 03:09:56 -0700

    string concat and true or false

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String[] args) {
+		String one="i am";
+		String two=" sakthiskv";
+		System.out.println(one+ two);
+		System.out.println(10<20);
+		System.out.println(20<10);
+		
+	 
+	}
+	
+
+}
