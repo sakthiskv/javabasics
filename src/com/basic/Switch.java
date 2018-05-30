d4faa81c57f7394add5d23547ac404710ad3159e
sakthiskv <romansakthi18@gmail.com>
2018-05-29 21:32:12 -0700

    Switch.java

---
+++ src/com/basic/Switch.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class Switch {
+	public static void main(String[] args) {
+	int n = 2;
+	switch (n)
+	{
+	case 1:
+	System.out.println( "MONA" );
+	case 2:
+	System.out.println( "Sakthivel;" );
+	break;
+	default:
+	System.out.println( "The number is not 1 or 2" );
+	break;
+	}
+	}
+
+
+
+}
