7f088e729e5a3a338146e2c1578a041f28b1249a
sakthiskv <romansakthi18@gmail.com>
2018-06-01 00:05:47 -0700

    static.java for bank account

--- src/com/basic/SavingAccount.java
+++ src/com/basic/SavingAccount.java
@@ -1,36 +1,36 @@
 package com.basic;
 class Smp{
-	static float a1=2000;
-	static float a2=3000;
-	static {
-		System.out.println("Account bal="+a1+ "Account bal="+a2);
-	}
-	public float cal() {
-		float s=(a1*4/100)*12;
-		return s;
-	}
+	static double interst;
 	
-	public float cal1() {
-		float b=(a1*5/100)*12;
-		return b;
+	private double balance;
+	
+	public Smp(float bal) {
+		this.balance = bal;
+	}
+	public void cal() {
+		double s=(balance*interst)/12;
+		balance = balance + s;
 		
 	}
-	public static void getcus() {
-		float m=(a2*4/100)*12;
-		System.out.println(" Saver2 Annuall interst 4% increase="+m);
-		float p=(a2*5/100)*12;
-		System.out.println(" Saver2 Annuall interst 5% increase="+p);
+	public static void setInterest(double interest) {
+		Smp.interst = interest;
+	}
+	public void getcus() {
+		System.out.println(" Saver2 Annuall interst 4% increase="+balance);
 	}
 	
 	
 	
 }
 public class SavingAccount {
+	
 	public static void main(String[] args) {
-Smp saver1=new Smp();
-Smp saver2=new Smp();
- System.out.println("saver1 Annuall interst 4% increase="+saver1.cal());
- System.out.println("saver1 Annuall interst 5% increase="+saver1.cal1());
+Smp saver1=new Smp(3000);
+Smp saver2=new Smp(200);
+Smp.setInterest(0.04);
+saver1.cal();
+saver1.getcus();
+ saver2.cal();
  saver2.getcus();
 	}
 }
